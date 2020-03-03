9b066bb62af51eb77b6e314df00c038fd57637c4
21smendoza <smendoza@ucvts.org>
2020-03-03 18:24:43 -0500

    Added in the background colors and the volleyball field.

--- game.py
+++ game.py
@@ -6,8 +6,15 @@ import math
 #initializes the program
 pygame.init()
 pygame.mixer.init()
+dark_brown = (102, 51, 0)
+light_brown = (153, 76, 0)
+lightest_brown = (255, 229, 204)
+black = (0, 0, 0)
+grey = (96, 96, 96)
 
-screen = pygame.display.set_mode( (800, 600) )
+width = 1100
+height = 650
+screen = pygame.display.set_mode( (width, height) )
 #internal clock creates a slight delay to prevent program from speeding
 clock = pygame.time.Clock()
 
@@ -23,5 +30,23 @@ while run:
         #exits the game when called to exit
         if event.type == pygame.QUIT:
             sys.exit()
+    
+    mouse_pos = pygame.mouse.get_pos()
+    print(mouse_pos)
+    
+    pygame.draw.rect(screen, light_brown, (0, 0, width, height / 1.3))
+    pygame.draw.rect(screen, dark_brown, (0, height / 1.3, width, height / 1.8))
+    pygame.draw.polygon(screen, lightest_brown, ((width / 9, 150), (width / 9 * 8, 150), (width / 22 * 21, 450), (width / 22, 450)))
+    pygame.draw.line(screen, black, (int(round(width / 2)), 150), (int(round(width / 2)), 450), 3)
+    pygame.draw.line(screen, black, (int(round(width / 4.8)), 150), (int(round(width / 6.2)), 450), 3)
+    pygame.draw.line(screen, black, (int(round(width / 3.3)), 150), (int(round(width / 3.7)), 450), 3)
+    pygame.draw.line(screen, black, (int(round(width / 2.5)), 150), (int(round(width / 2.6)), 450), 3)
+    pygame.draw.line(screen, black, (int(round(width / 1.655)), 150), (int(round(width / 1.63)), 450), 3)
+    pygame.draw.line(screen, black, (int(round(width / 1.261)), 150), (int(round(width / 1.19)), 450), 3)
+    pygame.draw.line(screen, black, (int(round(width / 1.43)), 150), (int(round(width / 1.385)), 450), 3)
+    pygame.draw.line(screen, black, (int(round(width / 10.6)), 225), (int(round(width / 1.105)), 225), 3)
+    pygame.draw.line(screen, black, (int(round(width / 12.8)), 300), (int(round(width / 1.085)), 300), 3)
+    pygame.draw.line(screen, black, (int(round(width / 16.2)), 375), (int(round(width / 1.067)), 375), 3)
+    pygame.draw.rect(screen, grey, (width / 2 - 10, 130, 20, 350))
 
     pygame.display.update()
